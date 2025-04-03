f2e68bedc902f7648050074ec2ca7c1fca9a978d
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-04 01:18:09 +0800

    Add game pseudocode

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// paper>rock rock>scissors scissors>paper
+
+// project type: console
+
+// input: n - number of rounds
+// output: Computer Wins, Human Wins
+
+// class Rock-Paper-Scissors{
+// attributes:
+//     let computerScore = 0;
+//     let humanScore = 0;
+// methods:
+//     getComputerChoice();
+//     getHumanChoice(string);
+//     playRound(humanChoice, computerChoice);
+//     playGame(rounds);
+// }
