8c544b732f8e3102b2c9390c0e7e6cc6b83a3898
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-05-18 17:40:39 +0800

    Add references to dynamic nodes

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,20 @@ function getComputerChoice() {
   const choice = choices[Math.floor(Math.random() * choices.length)];
   return choice;
 }
+
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === "paper" && humanChoice === "rock") return "computer";
+  else if (computerChoice === "rock" && humanChoice === "scissors")
+    return "computer";
+  else if (computerChoice === "scissors" && humanChoice === "paper")
+    return "computer";
+  else if (computerChoice === humanChoice) return "draw";
+  else return "human";
+}
+
+const humanChoiceDisplay = document.querySelector(".human-choice");
+const computerChoiceDisplay = document.querySelector(".computer-choice");
+const humanScoreDisplay = document.querySelector(".human-score");
+const computerScoreDisplay = document.querySelector(".computer-score");
+const humanChoices = document.querySelector(".choices");
+const newGame = document.querySelector(".new-game");
