46562d4e476a1759b0f8fe84e9905b3f61ca67df
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-05-19 20:36:39 +0800

    Add rounds logic

--- scripts/main.js
+++ scripts/main.js
@@ -18,30 +18,41 @@ const humanChoiceDisplay = document.querySelector(".human-choice");
 const computerChoiceDisplay = document.querySelector(".computer-choice");
 const humanScoreDisplay = document.querySelector(".human-score");
 const computerScoreDisplay = document.querySelector(".computer-score");
+const roundsDisplay = document.querySelector(".rounds p span");
 const humanChoices = document.querySelector(".choices");
 const newGame = document.querySelector(".new-game");
 
 let computerScore = 0;
 let humanScore = 0;
+let rounds = 5;
+
+roundsDisplay.textContent = rounds.toString();
 
 humanChoices.addEventListener("click", (e) => {
   const humanChoice = e.target.className.toString();
   const computerChoice = getComputerChoice();
+  if (rounds) {
+    const roundResult = playRound(computerChoice, humanChoice);
+    if (roundResult === "computer") computerScore++;
+    else if (roundResult === "human") humanScore++;
 
-  const roundResult = playRound(computerChoice, humanChoice);
-  if (roundResult === "computer") computerScore++;
-  else if (roundResult === "human") humanScore++;
+    computerChoiceDisplay.textContent = computerChoice;
+    humanChoiceDisplay.textContent = humanChoice;
+    computerScoreDisplay.textContent = computerScore.toString();
+    humanScoreDisplay.textContent = humanScore.toString();
 
-  computerChoiceDisplay.textContent = computerChoice;
-  humanChoiceDisplay.textContent = humanChoice;
-  computerScoreDisplay.textContent = computerScore.toString();
-  humanScoreDisplay.textContent = humanScore.toString();
+    roundsDisplay.textContent = `${rounds}`;
+    rounds -= 1;
+  }
+  roundsDisplay.textContent = `${rounds}`;
 });
 
 newGame.addEventListener("click", () => {
   computerScore = 0;
   humanScore = 0;
+  rounds = 5;
 
+  roundsDisplay.textContent = rounds.toString();
   computerChoiceDisplay.textContent = "?";
   humanChoiceDisplay.textContent = "?";
   computerScoreDisplay.textContent = computerScore.toString();
